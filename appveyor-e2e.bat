85adf02d8c42d63079786fda5600b8efcf3f26fd
Rafael Ribeiro <1150706@isep.ipp.pt>
2019-01-01 00:56:05 +0000

    .

--- appveyor-e2e.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF defined APPVEYOR_PULL_REQUEST_NUMBER (
-    node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/basic/**"
-) ELSE (
-    node tests\legacy-cli\run_e2e.js --appveyor "--glob=tests/{basic,commands,generate,build/styles}/**"
-)
